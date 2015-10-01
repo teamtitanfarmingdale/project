bdad3b688b3f726a0f6ad2f661bff65716dc2c0e
teamtitanfarmingdale <rojal@farmingdale.edu>
2015-10-01 19:04:10 -0400

    Initial Commit

---
+++ src/game/Player.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Player {
+	private BufferedImage[] movingLeft = {Sprite.getSprite(0, 1), Sprite.getSprite(2, 1}; 
+	private BufferedImage[] movingRight = {Sprite.getSprite(0, 2), Sprite.getSprite(2, 2};
+	private BufferedImage[] accelerate = {Sprite.getSprite(1, 0)};
+
+	// These are animation states
+	private Animation moveLeft = new Animation(movingLeft, 10);
+	private Animation moveRight = new Animation(movingRight, 10);
+	private Animation accelerate = new Animation(accelerate, 10);
+
+	// This is the actual animation
+	private Animation animation = standing;
+}
